d6adf3b5cfba1f0814118f1e198e57740c67ccd4
ggracelynn <157567457+ggracelynn@users.noreply.github.com>
2024-02-06 17:11:25 -0800

    Delete homework/hw1.py

--- homework/hw1.py
+++
@@ -1,5 +0,0 @@
-print(3.14**42+(13*1.29))
-#I'm hungry right now so that's why pi is there
-#42 is the answer to life the universe and everything
-#13 is my lucky number
-#1.29 is today's date and I'm too tired to think of anything more clever 
