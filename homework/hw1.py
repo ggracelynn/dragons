06c744c9f8532f5f35521f9ed4f09ad8f7a49a37
ggracelynn <157567457+ggracelynn@users.noreply.github.com>
2024-02-06 17:04:11 -0800

    Add files via upload

---
+++ homework/hw1.py
@@ -0,0 +1,5 @@
+print(3.14**42+(13*1.29))
+#I'm hungry right now so that's why pi is there
+#42 is the answer to life the universe and everything
+#13 is my lucky number
+#1.29 is today's date and I'm too tired to think of anything more clever 
