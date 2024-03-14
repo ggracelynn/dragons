3d72c7996c1ac2dcd2fcc84dd9ccde7e2b834a7b
ggracelynn <157567457+ggracelynn@users.noreply.github.com>
2024-03-13 21:20:03 -0700

    Add files via upload

---
+++ homework/homework3/bmi.py
@@ -0,0 +1,9 @@
+def bmi(weight, height):
+    
+    return 703 * weight / (height**2)
+
+weight=160
+height= 70 #height is in inches 
+#this should return a bmi of 22.955 according to bmi calculators 
+print(bmi(weight,height))
+
