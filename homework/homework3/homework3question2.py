ba557b4e95e2822e5bb036f86cd24f2501419238
ggracelynn <157567457+ggracelynn@users.noreply.github.com>
2024-03-13 20:37:26 -0700

    Add files via upload

---
+++ homework/homework3/homework3question2.py
@@ -0,0 +1,11 @@
+def smallest_and_largest (numbers):
+    return min(numbers), max(numbers)
+
+
+
+numbers = [5,12,17,3,2,81]
+print((min(numbers),max(numbers)))
+
+
+#should return an ordered pair (tuple) with the smallest value first and the largest second. 
+#in the case of this example, it should print (2,81)
